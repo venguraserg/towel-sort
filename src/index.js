63366f85000334c7b03d982e2adec36bf8c01b3c
venguraserg <venguraserg@gmail.com>
2023-08-15 14:18:40 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 
 // You should implement your task here.
+// const matrix1= NaN;
+// towelSort1(matrix1);
 
-module.exports = function towelSort (matrix) {
-  return [];
+
+
+
+module.exports = function towelSort(matrix) {
+  let newArray = [];
+  if (!Array.isArray(matrix)) return [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 == 0) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        newArray.push(matrix[i][j]);
+      }
+    }
+    else {
+      for (let j = matrix[i].length - 1; j >= 0; j--) {
+        newArray.push(matrix[i][j]);
+      }
+    }
+  }
+  return newArray;
 }
+
+
+// function towelSort1(matrix) {
+//   let newArray = [];
+//   console.log(matrix);
+//   for (let i = 0; i < matrix.length; i++) {
+//     if (i % 2 == 0) {
+//       for (let j = 0; j < matrix[i].length; j++) {
+//         newArray.push(matrix[i][j]);
+//       }
+//     }
+//     else {
+//       for (let j = matrix[i].length-1; j >= 0; j--) {
+//         newArray.push(matrix[i][j]);
+//       }
+//     }
+//   }
+//   console.log(newArray);
+// }
